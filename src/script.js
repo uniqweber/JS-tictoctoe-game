cf2c9ad4d3b77554b272bde656dc655766931b5d
rayanahmed <thismac@MacBookAir.Dlink>
2025-01-05 04:38:06 -0800

    tailwind file rechecked

---
+++ src/script.js
@@ -0,0 +1,15 @@
+const myLove = {
+  name: "Shraboni",
+  age: 20,
+  education: "honrs",
+  isMarried: true,
+};
+console.log("🚀 ~ myLove:", myLove)
+console.log("🚀 ~ myLove:", myLove)
+console.log("🚀 ~ myLove:", myLove)
+console.log("🚀 ~ myLove:", myLove)
+console.log("🚀 ~ myLove:", myLove)
+console.log("🚀 ~ myLove:", myLove)
+console.log("🚀 ~ myLove:", myLove)
+console.log("🚀 ~ myLove:", myLove)
+console.log(myLove);
