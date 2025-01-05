02bb2f112b7ae2c65b31b2baff277bd4bb946706
rayanahmed <thismac@MacBookAir.Dlink>
2025-01-06 01:45:01 +0600

    ui part 80% done

---
+++ styles/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/* const wrapper = document.querySelector("#wrapper");
+wrapper.addEventListener("click", (e) => {
+  if (e.target.innerText == "") {
+    e.target.innerText = "O";
+    e.target.style.color = "red";
+    e.target.setAttribute = "disabled";
+  }
+  if (e.target.innerText == "") {
+    e.target.innerText = "O";
+    e.target.style.color = "red";
+  }
+});
+ */
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "serif"],
+        // Add more custom font families as needed
+      },
+      colors: {
+        dark: "#0d1b2a",
+        "dark-accent": "#1b263b",
+        light: "#e0e1dd",
+        "light-accent": "#415a77",
+      },
+    },
   },
   plugins: [],
 };
